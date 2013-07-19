d72e3612c4a20a11dec89f5eca126759d92aaef3
Lalo Izquierdo <lalo.izq@gmail.com>
2013-07-19 22:37:41 +0000

    Buffer added

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
+var buff = fs.readFileSync(index.html);
+var hello = buf.toString('utf8',0,buff);
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(hello);
 });
 
 var port = process.env.PORT || 5000;
